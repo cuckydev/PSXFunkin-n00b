e1b7b9795a4ff0b94ba441e7b144e0c01f095cd2
Regan Green <regan_green27@hotmail.com>
2022-03-20 04:15:26 -0400

    sure lets license public.h why the fuck not

--- src/Public.h
+++ src/Public.h
@@ -1,3 +1,14 @@
+/*
+ * [PSXFunkin-n00b]
+ *   Public.h
+ * Author(s): Regan Green
+ * Date: 03/20/2022
+
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+*/
+
 PUBLIC(printf)
 
 PUBLIC(Main::LibraryNext)
